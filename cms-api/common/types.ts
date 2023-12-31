34a0a5331efc14ced0c4ea6a50bbdba4bc3b5441
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-31 16:04:23 +0400

    create interfaces for models

---
+++ cms-api/common/types.ts
@@ -0,0 +1,27 @@
+import { Types } from "mongoose";
+
+export interface iUser {
+  name: string;
+  email: string;
+  password: string;
+  comments: Types.ObjectId[];
+  posts: Types.ObjectId[];
+}
+
+export interface iComment {
+  text: string;
+  timestamp: Date;
+  post: Types.ObjectId;
+  author: Types.ObjectId;
+}
+
+export interface iPost {
+  title: string;
+  summary: string;
+  tags: string[];
+  body: string;
+  category: string;
+  timestamp: Date;
+  comments: Types.ObjectId[];
+  author: Types.ObjectId;
+}
