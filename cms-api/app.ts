bb0d98e82fd20bd25f6a09e11d0e0b6052618b78
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-01 11:20:22 +0400

    connect to mongoose and setup routes

--- cms-api/app.ts
+++ cms-api/app.ts
@@ -1,17 +1,34 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
+import postRouter from "./routes/posts";
+import commentRouter from "./routes/comments";
+import userRouter from "./routes/users";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  if (!process.env.MONGO_STRING) {
+    console.error("Invalid mongo string");
+    return;
+  }
+  await mongoose.connect(process.env.MONGO_STRING);
+}
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/v1", indexRouter);
-app.use("/v1/users", usersRouter);
+app.use("/v1/posts", postRouter);
+app.use("/v1/comments", commentRouter);
+app.use("/v1/users", userRouter);
 
 export default app;
