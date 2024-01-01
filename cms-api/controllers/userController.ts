a92926a13368fa7ff6d10400b8a18f0b29662427
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-01 11:21:02 +0400

    complete user controller

--- cms-api/controllers/userController.ts
+++ cms-api/controllers/userController.ts
@@ -5,11 +5,19 @@ import asyncHandler from "express-async-handler";
 const userProjection = { __v: 0, password: 0 };
 
 export const user_list = asyncHandler(async (req, res, next) => {
-  const users = await User.find({}, userProjection);
-  res.json(users);
+  try {
+    const users = await User.find({}, userProjection);
+    res.json(users);
+  } catch (error) {
+    res.status(404).json({ error: "Users not found." });
+  }
 });
 
 export const user_detail = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id, userProjection).exec();
-  res.json(user);
+  try {
+    const user = await User.findById(req.params.id, userProjection).exec();
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ error: "User not found." });
+  }
 });
