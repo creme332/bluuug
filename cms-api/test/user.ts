78e2b9a359bc7e4f7599df6bf467caea12f6352e
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-03 13:49:43 +0400

    test read and create routes

--- cms-api/test/user.ts
+++ cms-api/test/user.ts
@@ -1,27 +1,43 @@
 import request from "supertest";
 import app from "../app";
+import assert from "assert";
+import { iUser } from "../common/types";
 
 describe("GET /v1/users", function () {
-  it("responds with json", function (done) {
+  it("responds with json", async function () {
     request(app)
       .get("/v1/users")
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      //   .expect({ name: "frodo" })
-      .expect(200, done);
+      .expect(200)
+      .end((err, res) => {
+        assert(res.body.length > 0);
+        assert.deepStrictEqual(
+          new Set(res.body.map((e: iUser) => e.name)),
+          new Set(["creme332", "test_user", "harry kane"])
+        );
+      });
   });
 });
 
-// describe("POST /v1/users/create", function () {
-//   it("responds with json", function (done) {
-//     request(app)
-//       .post("/v1/users/create")
-//       .type("form")
-//       .send({ item: "hey" })
-//       .then(() => {
-//         request(app)
-//           .get("/v1/users/")
-//           .expect({ array: ["hey"] }, done);
-//       });
-//   });
-// });
+describe("POST /v1/users/create", function () {
+  it("saves user to database", function () {
+    request(app)
+      .post("/v1/users/create")
+      .type("form")
+      .send({
+        email: "pppp@gmail.com",
+        password: "hello33",
+        name: "test_user_999",
+      })
+      .then(() => {
+        request(app)
+          .get("/v1/users/")
+          .end((err, res) => {
+            assert(
+              res.body.map((e: iUser) => e.name).includes("test_user_999")
+            );
+          });
+      });
+  });
+});
