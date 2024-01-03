03105e8df70333bac6756eed9dbdfc61b3d2399d
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-03 13:42:48 +0400

    refactor

--- cms-api/database/populate.ts
+++ cms-api/database/populate.ts
@@ -4,43 +4,27 @@
  */
 
 import dotenv from "dotenv";
-import debug from "debug";
 import mongoose from "mongoose";
-import { createUsers, createPosts, createComments } from "./data";
+import dataManager from "./data";
 
 dotenv.config();
-const mongoDebug = debug("mongo");
-const createUsersDebug = debug("createUsers");
-const createPostsDebug = debug("createPosts");
-const createCommentsDebug = debug("createComments");
 
 mongoose.set("strictQuery", false);
-main().catch((err) => mongoDebug(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  mongoDebug("About to connect");
+  console.log("About to connect");
 
   if (!process.env.MONGO_STRING) {
-    mongoDebug("MONGO_STRING is missing from .env file");
+    console.log("MONGO_STRING is missing from .env file");
     return;
   }
 
   await mongoose.connect(process.env.MONGO_STRING);
-  mongoDebug("Connected");
+  console.log("Connected");
+  dataManager().save();
 
-  createUsersDebug("Creating users");
-  await createUsers();
-  createUsersDebug("Finished.");
-
-  createPostsDebug("Creating posts");
-  await createPosts();
-  createPostsDebug("Finished");
-
-  createCommentsDebug("Creating comments");
-  await createComments();
-  createCommentsDebug("Finished");
-
-  mongoDebug("Closing mongoose");
+  console.log("Closing mongoose");
 
   mongoose.connection.close();
 }
