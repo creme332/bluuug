9d59eee81314a57b13813219899d39649a9f372a
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-04 13:27:22 +0400

    delete database after each test

--- cms-api/test/hooks.ts
+++ cms-api/test/hooks.ts
@@ -1,5 +1,6 @@
 import testServer from "../database/mongoConfigTesting";
 import dataManager from "../database/data";
+import mongoose from "mongoose";
 
 const mongoServer = testServer(); //
 
@@ -8,7 +9,16 @@ export const mochaHooks = {
   beforeAll: [
     async function () {
       await mongoServer.initialize();
+    },
+  ],
+  beforeEach: [
+    async function () {
       await dataManager().save();
+    },
+  ],
+  afterEach: [
+    async function () {
+      await mongoose.connection.db.dropDatabase();
     },
   ],
   afterAll: [
