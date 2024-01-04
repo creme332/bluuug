b995df2bced80719445de322efb4786cc78e9452
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-04 12:49:45 +0400

    use async await

--- cms-api/test/user.ts
+++ cms-api/test/user.ts
@@ -5,41 +5,39 @@ import User from "../models/user";
 import { iUser } from "../common/types";
 
 describe("GET /v1/users", function () {
-  it("responds with an array of objects", function () {
-    request(app)
+  it("responds with an array of objects", async function () {
+    const res = await request(app)
       .get("/v1/users")
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      .expect(200)
-      .end((err, res) => {
-        assert(res.body.length > 0);
-        assert.deepStrictEqual(
-          new Set(res.body.map((e: iUser) => e.name)),
-          new Set(["creme332", "test_user", "harry kane"])
-        );
-      });
+      .expect(200);
+
+    assert(res.body.length > 0);
+
+    assert.deepStrictEqual(
+      new Set(res.body.map((e: iUser) => e.name)),
+      new Set(["creme332", "test_user", "harry kane"])
+    );
   });
 });
 
 describe("POST /v1/users/create", function () {
-  it("saves user to database", function () {
-    request(app)
+  it("saves user to database", async function () {
+    await request(app)
       .post("/v1/users/create")
       .type("form")
       .send({
         email: "pppp@gmail.com",
-        password: "hello33",
+        password: "hellogdfg33",
         name: "test_user_999",
       })
-      .then(() => {
-        request(app)
-          .get("/v1/users/")
-          .end((err, res) => {
-            assert(
-              !res.body.map((e: iUser) => e.name).includes("test_user_999")
-            );
-          });
-      });
+      .expect(200);
+
+    const getResponse = await request(app).get("/v1/users/").expect(200);
+
+    assert(
+      getResponse.body.map((e: iUser) => e.name).includes("test_user_999")
+    );
   });
 });
 
@@ -52,17 +50,10 @@ describe("POST /v1/users/delete without authentication", function () {
     });
     await user.save();
 
-    await request(app)
-      .post(`/v1/users/${user.id}/delete`)
-      .then(() => {
-        request(app)
-          .get("/v1/users/")
-          .end((err, res) => {
-            console.log(res.body);
-            assert(
-              !res.body.map((e: iUser) => e.name).includes("deleted_user")
-            );
-          });
-      });
+    await request(app).post(`/v1/users/${user.id}/delete`).expect(200);
+
+    const res = await request(app).get("/v1/users/");
+
+    assert(!res.body.map((e: iUser) => e.name).includes("deleted_user"));
   });
 });
