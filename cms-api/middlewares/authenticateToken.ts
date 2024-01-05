11fdecebbd7c4343d9e906a7b41db93ce9a98208
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-05 15:43:40 +0400

    deserialize token and pass to middleware

--- cms-api/middlewares/authenticateToken.ts
+++ cms-api/middlewares/authenticateToken.ts
@@ -1,7 +1,24 @@
 import dotenv from "dotenv";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import User from "../models/user";
+
 dotenv.config();
+
+function parseJwt(token: string) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
 
 const authenticateToken = async (
   req: Request,
@@ -16,7 +33,7 @@ const authenticateToken = async (
     return;
   }
 
-  // verify token 
+  // verify token
   if (!process.env.ACCESS_TOKEN_SECRET) {
     console.error("ACCESS_TOKEN_SECRET is not defined");
     res.sendStatus(403);
@@ -25,6 +42,12 @@ const authenticateToken = async (
 
   try {
     await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const deserializedToken = parseJwt(token);
+
+    // pass user document to next middleware
+    const user = await User.findById(deserializedToken.id);
+    res.locals.user = user;
     next();
   } catch (error) {
     res.sendStatus(403);
