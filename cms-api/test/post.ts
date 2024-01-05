650e7fb99b718407ad88ec94ab1f9c645869e980
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-05 15:43:16 +0400

    add tests for user authentication

--- cms-api/test/post.ts
+++ cms-api/test/post.ts
@@ -5,8 +5,13 @@ import User from "../models/user";
 import Post from "../models/post";
 import { iPost } from "../common/types";
 
+const sampleUser = {
+  email: "creme332@bluuug.com",
+  password: "sample_password",
+};
+
 describe("GET /v1/posts", function () {
-  it("responds with an array of objects", async function () {
+  it("responds with an array of posts", async function () {
     const res = await request(app)
       .get("/v1/posts")
       .set("Accept", "application/json")
@@ -22,11 +27,8 @@ describe("GET /v1/posts", function () {
 });
 
 describe("POST /v1/posts/create", async function () {
-  it("saves post to database", async function () {
-    const userResponse = await request(app).get("/v1/users");
-    const userID = userResponse.body[0]._id;
-
-    const res = await request(app)
+  it("forbid creation by unauthenticated user", async function () {
+    await request(app)
       .post("/v1/posts/create")
       .type("form")
       .send({
@@ -34,25 +36,73 @@ describe("POST /v1/posts/create", async function () {
         body: "this is the body",
         category: "test_category",
         summary: "this is a summary",
-        author: userID,
+      })
+      .expect(401);
+  });
+
+  it("allow creation by authenticated user", async function () {
+    // get access token through login
+    const loginResponse = await request(app)
+      .post("/v1/auth/login")
+      .type("form")
+      .send(sampleUser)
+      .expect(200);
+
+    // create a post
+    await request(app)
+      .post("/v1/posts/create")
+      .set("Authorization", `Bearer ${loginResponse.body.accessToken}`)
+      .type("form")
+      .send({
+        title: "unique_authenticated_post",
+        body: "this is the body",
+        category: "test_category",
+        summary: "this is a summary",
       })
       .expect(200);
 
+    // fetch all posts from database
     const getResponse = await request(app).get("/v1/posts/").expect(200);
-
-    assert(
-      getResponse.body.map((e: iPost) => e.title).includes("Test post 999")
+    const matchingPosts = getResponse.body.filter(
+      (e: iPost) => e.title === "unique_authenticated_post"
     );
+
+    assert(matchingPosts.length == 1);
+    assert(matchingPosts[0].body === "this is the body");
+    assert(matchingPosts[0].category === "test_category");
+    assert(matchingPosts[0].summary === "this is a summary");
   });
 });
 
-describe("POST /v1/posts/delete without authentication", function () {
-  it("deletes post from database", async function () {
-    const user = new User({
-      email: "dsademail@email.com",
-      name: "user3433",
-      password: "hashedPassword",
+describe("POST /v1/posts/delete", function () {
+  it("forbid deletion by unauthenticated user", async function () {
+    const user = (
+      await User.find({
+        email: sampleUser.email,
+      })
+    )[0];
+
+    // create a new post to be deleted
+    const post = new Post({
+      title: "delete_post",
+      body: "this is the body",
+      category: "test_category",
+      summary: "this is a summary",
+      timestamp: new Date(),
+      author: user._id,
     });
+
+    await post.save();
+
+    await request(app).post(`/v1/posts/${post.id}/delete`).expect(401);
+  });
+
+  it("allow deletion by authenticated user", async function () {
+    const user = (
+      await User.find({
+        email: sampleUser.email,
+      })
+    )[0];
 
     const post = new Post({
       title: "delete_post",
@@ -60,14 +110,21 @@ describe("POST /v1/posts/delete without authentication", function () {
       category: "test_category",
       summary: "this is a summary",
       timestamp: new Date(),
-      author: user.id,
+      author: user._id,
     });
 
-    await user.save();
     await post.save();
+
+    // get access token through login
+    const loginResponse = await request(app)
+      .post("/v1/auth/login")
+      .type("form")
+      .send(sampleUser)
+      .expect(200);
 
     await request(app)
       .post(`/v1/posts/${post.id}/delete`)
+      .set("Authorization", `Bearer ${loginResponse.body.accessToken}`)
       .then(() => {
         request(app)
           .get("/v1/posts/")
